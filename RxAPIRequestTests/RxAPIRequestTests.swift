f20416b6983e29f7c17a423afc3baaad0bd83289
shtnkgm <nakagami.shota@gmail.com>
2018-09-28 20:11:09 +0900

    Initial Commit

---
+++ RxAPIRequestTests/RxAPIRequestTests.swift
@@ -0,0 +1,34 @@
+//
+//  RxAPIRequestTests.swift
+//  RxAPIRequestTests
+//
+//  Created by Shota Nakagami on 2018/09/28.
+//  Copyright © 2018年 Shota Nakagami. All rights reserved.
+//
+
+import XCTest
+@testable import RxAPIRequest
+
+class RxAPIRequestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
