443c6c42465e03504eee61c59fdc5f9848ae9af9
shtnkgm <nakagami.shota@gmail.com>
2018-09-29 15:55:56 +0900

    fix

--- RxAPIRequest/ViewController.swift
+++ RxAPIRequest/ViewController.swift
@@ -6,6 +6,9 @@
 //  Copyright © 2018年 Shota Nakagami. All rights reserved.
 //
 
+import Alamofire
+import RxCocoa
+import RxSwift
 import UIKit
 
 /**
@@ -17,6 +20,8 @@ import UIKit
 final class ViewController: UIViewController {
     @IBOutlet private weak var requestButton: UIButton!
 
+    private let disposeBag = DisposeBag()
+
     init() {
         super.init(nibName: nil, bundle: nil)
     }
@@ -27,5 +32,28 @@ final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        requestButton.rx.tap.subscribe(onNext: { [weak self] in
+            print("リクエストボタンタップ")
+            self?.getUserInfo()
+        }).disposed(by: disposeBag)
+    }
+
+    func getUserInfo() {
+        let urlString = "https://raw.githubusercontent.com/shtnkgm/RxAPIRequest/master/RxAPIRequest/API/user_info_api.json"
+        Alamofire.request(urlString, method: .get).responseJSON { response in
+            guard let data = response.data else {
+                print("データの取得に失敗: \(response.response?.statusCode ?? 0)")
+                return
+            }
+            do {
+                let jsonDecoder = JSONDecoder()
+                jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+                let userInfo = try jsonDecoder.decode(UserInfo.self, from: data)
+                print("データの取得に成功: \(userInfo)")
+            } catch {
+                print("JSONのデコードに失敗: \(error)")
+            }
+        }
     }
 }
